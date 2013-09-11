6746ea9be72a7cc4e35f0596400da8152be66f9a
MightyBear007 <mightybear007@gmail.com>
2013-09-11 14:01:35 +0530

    Update vanir.mk

--- vanir.mk
+++ vanir.mk
@@ -1,6 +1,2 @@
-$(call inherit-product, device/htc/m7ul/vanir_m7ul.mk)
+$(call inherit-product, device/htc/m7ul/full_m7ul.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=JWR66V.H10 BUILD_FINGERPRINT="htc/m7_google/m7:4.3/JWR66V.H10/230993:user/release-keys" PRIVATE_BUILD_DESC="3.06.1700.10 CL230993 release-keys"
-
-PRODUCT_NAME := vanir_m7ul
-PRODUCT_DEVICE := m7ul
